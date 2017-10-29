a1264a4dc2fc3eac773f3383e25f5c44e48f682b
Edmond Atto <edmond091@gmail.com>
2017-10-29 08:30:13 +0300

    Add onChange attribute for name change

--- src/components/GuestName.js
+++ src/components/GuestName.js
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types';
 const GuestName = props => {
   if (props.isEditing) {
     return (
-      <input type="text" value={props.children}/>
+      <input
+        type="text"
+        value={props.children}
+        onChange={props.handleNameEdit}
+      />
     )
   }
   return(
@@ -16,6 +20,7 @@ const GuestName = props => {
 
 GuestName.propTypes = {
   isEditing: PropTypes.bool.isRequired,
+  handleNameEdit: PropTypes.func.isRequired,
 };
 
 export default GuestName;
