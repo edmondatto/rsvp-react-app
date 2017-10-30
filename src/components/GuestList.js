875ca071517e842561d442fb4b35f6f27c2a0aad
Edmond Atto <edmond091@gmail.com>
2017-10-30 13:55:10 +0300

    Add pendingGuest component and propTypes to GuestList component

--- src/components/GuestList.js
+++ src/components/GuestList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Guest from "./Guest";
+import PendingGuest from './PendingGuest';
 
 const GuestList = props =>
   <ul>
+    <PendingGuest name={props.pendingGuest}/>
     {props.guests
       .filter(guest => !props.isFiltered || guest.isConfirmed)
       .map((guest,index) =>
@@ -26,6 +28,7 @@ GuestList.propTypes = {
   setNameAt: PropTypes.func.isRequired,
   isFiltered: PropTypes.bool.isRequired,
   removeGuestAt: PropTypes.func.isRequired,
+  pendingGuest: PropTypes.string.isRequired,
 };
 
 
